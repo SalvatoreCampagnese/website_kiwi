6f4fc68893251393c006d54c8664e98062a433ea
salvatore campagnese <salvatore.campagnese@gmail.com>
2025-01-15 17:29:52 +0100

    scaffolding

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+   'name': 'Kiwi Theme',
+   'description': Test
+   'category': 'Website/Theme',
+   'version': '15.0.0',
+   'author': 'Nebula S.r.l',
+   'license': 'AGPL-3',
+   'depends': ['website'],
+   'data': [],
+   'assets': {
+      'web._assets_primary_variables': [
+      ('prepend', 'website_airproof/static/src/scss/primary_variables.scss'),
+   ],
+   },
+}

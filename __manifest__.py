9111f5be726a6c76b77c45d0753538627c8dd5bf
SalvatoreCampagnese <salvatore.campagnese@gmail.com>
2025-02-26 21:05:52 +0100

    Implemented components

--- __manifest__.py
+++ __manifest__.py
@@ -12,6 +12,13 @@
         'views/snippets/s_kiwi_quickactions.xml',
         'views/snippets/s_kiwi_heroslider.xml',
         'views/snippets/s_kiwi_productslider.xml',
+        'views/snippets/s_kiwi_pointsbanner.xml',
+        'views/snippets/s_kiwi_infobox.xml',
+         'views/snippets/s_kiwi_learninginfo.xml',
+         'views/snippets/s_kiwi_bigbanner.xml',
+         'views/snippets/s_kiwi_twocolumn.xml',
+         'views/snippets/s_kiwi_premiumtiers.xml',
+         'views/snippets/s_kiwi_bigcards.xml',
 
         'views/website_templates.xml',
         'views/login_templates.xml',
@@ -30,10 +37,19 @@
          'kiwi_theme/static/src/scss/snippets/s_kiwi_quickactions.scss',
          'kiwi_theme/static/src/scss/snippets/s_kiwi_heroslider.scss',
          'kiwi_theme/static/src/scss/snippets/s_kiwi_productslider.scss',
+         'kiwi_theme/static/src/scss/snippets/s_kiwi_pointsbanner.scss',
+         'kiwi_theme/static/src/scss/snippets/s_kiwi_infobox.scss',
+         'kiwi_theme/static/src/scss/snippets/s_kiwi_learninginfo.scss',
+         'kiwi_theme/static/src/scss/snippets/s_kiwi_bigbanner.scss',
+         'kiwi_theme/static/src/scss/snippets/s_kiwi_twocolumn.scss',
+         'kiwi_theme/static/src/scss/snippets/s_kiwi_premiumtiers.scss',
+         'kiwi_theme/static/src/scss/snippets/s_kiwi_bigcards.scss',
          
          'kiwi_theme/static/src/img/wbuilder/s_kiwi_snippet.svg',
+         '/kiwi_theme/static/src/img/pointsbanner/coins.png'
          'kiwi_theme/static/src/img/login.png',
          'kiwi_theme/static/src/img/logo.svg',
+         'kiwi_theme/static/src/img/premiumtiers/check.svg',
 
          'kiwi_theme/static/src/js/hide.js',
       ],
